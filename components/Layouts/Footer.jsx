9fa10049c18a8ea1614d0fa7b1a54112f9e2625d
Paul Gabov <pgabow@gmail.com>
2023-05-25 11:29:19 +0300

    brought some beauty

--- components/Layouts/Footer.jsx
+++ components/Layouts/Footer.jsx
@@ -3,10 +3,10 @@ import { FaMapMarkedAlt, FaEnvelope, FaUserTie, FaPhoneAlt } from 'react-icons/f
 
 const Footer = () => {
     return (
-        <footer className='bg supports-backdrop-blur:bg-white/95 dark:bg-zinc-900/75 border-t border-sky-900 px-4 pt-8 md:px-12 lg:px-16 relative bottom-0 left-0 right-0 overflow-hidden'>
-            <div className='mt-8 max-w-7xl m-auto'>
-                <p className='text-center text-gray-500 dark:text-gray-400 py-4'> © { new Date().getFullYear() } - All Rights Not Reserved</p>
-            </div>
+        <footer className='bg supports-backdrop-blur:bg-white/95 dark:bg-zinc-900/75 border-t border-sky-900 px-1 pt-8 md:px-8 lg:px-10 relative bottom-0 left-0 right-0 overflow-hidden'>
+                <p className='text-center text-gray-500 dark:text-gray-400 pb-8'> © { new Date().getFullYear() } - All Rights Not Reserved</p>
+            {/* <div className='mt-8 max-w-7xl m-auto'>
+            </div> */}
         </footer>
     )
 }
